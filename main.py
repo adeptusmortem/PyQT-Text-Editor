21f21a7b1a20072efb4ab0b3fcea46545e907010
adeptusmortem <cellengoole@gmail.com>
2025-03-10 11:18:59 +0300

    modified:   main.py # Добавлен обработчик открытия

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QTextEdit, QMainWindow, QAction, QFileDialog
+from PyQt5.QtWidgets import QApplication, QLabel, QTextEdit, QMainWindow, QAction, QFileDialog, QMessageBox
 
 class TextEditor(QMainWindow):
     def __init__(self):
@@ -7,6 +7,14 @@ class TextEditor(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.title_text = 'Текстовый редактор'
+        self.setWindowTitle(self.title_text)
+        self.setGeometry(100, 100, 800, 600)
+
+        self.is_saved = True
+        self.current_file = None
+
+
         self.text_edit = QTextEdit(self)
         self.setCentralWidget(self.text_edit)
 
@@ -22,23 +30,44 @@ class TextEditor(QMainWindow):
         save_action.triggered.connect(self.save_file)
         file_menu.addAction(save_action)
 
+        save_as_action = QAction('Сохранить как', self)
+        save_as_action.triggered.connect(self.save_as_file)
+        file_menu.addAction(save_as_action)
+
         close_action = QAction('Закрыть', self)
         close_action.triggered.connect(self.close)
         file_menu.addAction(close_action)
 
-
-        self.setWindowTitle('Текстовый редактор')
-        self.setGeometry(100, 100, 800, 600)
+        create_action = QAction('Создать', self)
+        create_action.triggered.connect(self.create_file)
+        file_menu.addAction(create_action)
+        self.text_edit.textChanged.connect(self.on_text_changeed)
 
     def open_file(self):
         """
         Открывает файл для редактирования.
         """
         options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Открыть файлы", "", "(*.txt);;(*)", options=options)
+        if file_name:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    self.text_edit.setText(file.read())
+                    self.current_file = file_name
+                    self.is_saved = True
+                    self.setWindowTitle(f'{file_name} - {self.title_text}')
+            except Exception as e:
+                QMessageBox.critical(self, f"Ошибка при откытии файла:\n{e}")
 
     def save_file(self):
         """
         Сохраняет файл.
+        """
+        options = QFileDialog.Options()
+
+    def save_as_file(self):
+        """
+        Сохраняет файл как.
         """
         options = QFileDialog.Options()
 
@@ -47,6 +76,20 @@ class TextEditor(QMainWindow):
         Закрывает файл.
         """
         options = QFileDialog.Options()
+
+    def create_file(self):
+        """
+        Создать файл.
+        """
+        options = QFileDialog.Options()
+
+    def on_text_changeed(self):
+        """
+        Обработчик изменения текста.
+        """
+        if self.is_saved:
+            self.is_saved = False
+
 
 
 
